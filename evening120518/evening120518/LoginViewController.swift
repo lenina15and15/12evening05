72e986550207c123c6d2e527a95ad6cae70e92fd
Admin <lenina15i25@gmail.com>
2018-05-13 00:55:36 +0300

    Self
    login module

---
+++ evening120518/evening120518/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  evening120518
+//
+//  Created by Admin on 13/05/2018.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func loginAction(_ sender: UIButton) {
+        print("login")
+    }
+    
+    @IBAction func registrationAction(_ sender: UIButton) {
+        print("registration")
+    }
+    @IBAction func logoutAction(_ sender: UIButton) {
+        print("logout")
+    }
+}
